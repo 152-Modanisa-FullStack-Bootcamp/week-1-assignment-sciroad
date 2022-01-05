932ca1b6bfaab98e41a650bb5b94a9ee52fd83a9
sciroad <m.enes.kaplan0@gmail.com>
2022-01-05 23:36:05 +0300

    js-practice

--- src/app.js
+++ src/app.js
@@ -4,21 +4,36 @@ import axios from "axios";
 axios
   .get("https://my-json-server.typicode.com/modanisatech/bootcamp-db/products")
   .then((response) => {
-    // Firstly, log response to the console,
+    console.log(response);
     // inspect the response and see that it has data field
 
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
-    const products = null;
+    const products = response.data;
 
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
-
+    products.forEach((product) => console.log(product));
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+    const mappedProducts = products
+      .filter((product) => product.name.includes("Şal"))
+      .map((product) => ({ image: product.image, name: product.name }));
+    console.log(mappedProducts);
+
+    document.getElementById("container").innerHTML = mappedProducts
+      .map(
+        (product) =>
+          `
+          <div class="product">
+            <image src=${product.image}>
+            <span>${product.name}</span>
+          </div>
+          `
+      )
+      .join("");
 
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
